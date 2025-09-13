144e6b2e079cda2c1166b96b65019c225b536875
Angel Mena Coudin <kratosmorze@gmail.com>
2025-09-13 02:01:39 -0600

    Formulario para agregar un nuevo país a la base de datos

--- src/components/CountriesList.vue
+++ src/components/CountriesList.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="continer mt-5">
     <h1 class="display-4 text-center">Lista de países</h1>
+    <div class="row jutify-content-end">
+      <div class="col=2">
+        <a href="/country">
+          <button type="button" class="btn btn-outline-secondary float-right"> Agregar país</button>
+        </a>
+      </div>
+    </div>
     <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
       <thead>
         <tr>
