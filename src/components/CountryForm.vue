19cacb25e114af9b0378b8b211b1634ba170b3a2
Angel Mena Coudin <kratosmorze@gmail.com>
2025-09-13 00:54:08 -0600

    Agregando rutas al front-end. Laboratorio #5

---
+++ src/components/CountryForm.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Formulario para la creación de un país</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    setup() {
+      return{};
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
